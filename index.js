74946da0259a62bf0fbdea6cb92125d4202064a1
switchlove <switchbladelove@gmail.com>
2017-06-28 18:38:05 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 var request = require('request');
 var Discord = require('discord.io');
+var coinTicker = require('coin-ticker');
 var data = require("./config.json");
 
 botToken = data.botToken;
@@ -23,224 +24,152 @@ bot.on('message', function(user, userID, channelID, message, event) {
 	if (message === '.help') {
 		bot.sendMessage({
 			to: channelID,
-			message: "Checkable currencies: '.btc, .eth, .etc, .dash, .ltc, .gno, .rep, .xrp'"
+			message: "Checkable currencies: '.btc, .dash, .doge, .eth, .etc, .gno, .gnt, .ltc, .rep, .sc, .strat, .xrp'"
 		});	
 	}
 
-	if (message === ".btc") {
-		var url = 'https://api.kraken.com/0/public/Ticker?pair=xbtusd';
-		var msg='EMPTY';
-		request.get({
-			url: url,
-			json: true,
-			headers: {'User-Agent': 'request'}
-		}, (err, res, data) => {
-			if (err) {
-				console.log('Error:', err);
-				msg='Error '+ err;
-			} else if (res.statusCode !== 200) {
-				msg = 'Error Status code : '+res.statusCode;
-				console.log('Status:', res.statusCode);
-			} else {
-		      // data is already parsed as JSON:
-		      console.log(data.result.XXBTZUSD.a[0]);
-		      msg = data.result.XXBTZUSD.a[0]+'';
-		      console.log(msg);
-		      bot.sendMessage({
-		      	to: channelID,
-		      	message: ("BTC/USD : " + msg)
+	if (message==='.btc') {
+		coinTicker('bittrex', 'BTC_USD')
+   		.then((tick) => {
+      			console.log(tick)
+ 			console.log("BTC/USD: " + tick.last);
+			bot.sendMessage({
+		      	  to: channelID,
+		      	  message: ("BTC/USD: " + tick.last)
 		      });
-		  }
-		});
+   		})
 	}
 	
 	if (message==='.eth') {
-		var url = 'https://api.kraken.com/0/public/Ticker?pair=ethusd';
-		var msg='EMPTY';
-		request.get({
-			url: url,
-			json: true,
-			headers: {'User-Agent': 'request'}
-		}, (err, res, data) => {
-			if (err) {
-				console.log('Error:', err);
-				msg='Error '+ err;
-			} else if (res.statusCode !== 200) {
-				msg = 'Error Status code : '+res.statusCode;
-				console.log('Status:', res.statusCode);
-			} else {
-		      // data is already parsed as JSON:
-		      console.log(data.result.XETHZUSD.a[0]);
-		      msg = data.result.XETHZUSD.a[0]+'';
-		      console.log(msg);
-		      bot.sendMessage({
-		      	to: channelID,
-		      	message: ("ETH/USD : " + msg)
+		coinTicker('bittrex', 'ETH_BTC')
+   		.then((tick) => {
+      			console.log(tick)
+ 			console.log("ETH/BTC: " + tick.last);
+			bot.sendMessage({
+		      	  to: channelID,
+		      	  message: ("ETH/BTC: " + tick.last)
 		      });
-		  }
-		});
+   		})
 	}
-	
+
 	if (message==='.etc') {
-		var url = 'https://api.kraken.com/0/public/Ticker?pair=etcusd';
-		var msg='EMPTY';
-		request.get({
-			url: url,
-			json: true,
-			headers: {'User-Agent': 'request'}
-		}, (err, res, data) => {
-			if (err) {
-				console.log('Error:', err);
-				msg='Error '+ err;
-			} else if (res.statusCode !== 200) {
-				msg = 'Error Status code : '+res.statusCode;
-				console.log('Status:', res.statusCode);
-			} else {
-		      // data is already parsed as JSON:
-		      console.log(data.result.XETCZUSD.a[0]);
-		      msg = data.result.XETCZUSD.a[0]+'';
-		      console.log(msg);
-		      bot.sendMessage({
-		      	to: channelID,
-		      	message: ("ETC/USD : " + msg)
+		coinTicker('bittrex', 'ETC_BTC')
+   		.then((tick) => {
+      			console.log(tick)
+ 			console.log("ETC/BTC: " + tick.last);
+			bot.sendMessage({
+		      	  to: channelID,
+		      	  message: ("ETC/BTC: " + tick.last)
 		      });
-		  }
-		});
+   		})
 	}
-	
+
 	if (message==='.dash') {
-		var url = 'https://api.kraken.com/0/public/Ticker?pair=dashusd';
-		var msg='EMPTY';
-		request.get({
-			url: url,
-			json: true,
-			headers: {'User-Agent': 'request'}
-		}, (err, res, data) => {
-			if (err) {
-				console.log('Error:', err);
-				msg='Error '+ err;
-			} else if (res.statusCode !== 200) {
-				msg = 'Error Status code : '+res.statusCode;
-				console.log('Status:', res.statusCode);
-			} else {
-		      // data is already parsed as JSON:
-		      console.log(data.result.DASHUSD.a[0]);
-		      msg = data.result.DASHUSD.a[0]+'';
-		      console.log(msg);
-		      bot.sendMessage({
-		      	to: channelID,
-		      	message: ("DASH/USD : " + msg)
+		coinTicker('bittrex', 'DASH_BTC')
+   		.then((tick) => {
+      			console.log(tick)
+ 			console.log("DASH/BTC: " + tick.last);
+			bot.sendMessage({
+		      	  to: channelID,
+		      	  message: ("DASH/BTC: " + tick.last)
 		      });
-		  }
-		});
+   		})
 	}
-	
+
 	if (message==='.ltc') {
-		var url = 'https://api.kraken.com/0/public/Ticker?pair=ltcusd';
-		var msg='EMPTY';
-		request.get({
-			url: url,
-			json: true,
-			headers: {'User-Agent': 'request'}
-		}, (err, res, data) => {
-			if (err) {
-				console.log('Error:', err);
-				msg='Error '+ err;
-			} else if (res.statusCode !== 200) {
-				msg = 'Error Status code : '+res.statusCode;
-				console.log('Status:', res.statusCode);
-			} else {
-		      // data is already parsed as JSON:
-		      console.log(data.result.XLTCZUSD.a[0]);
-		      msg = data.result.XLTCZUSD.a[0]+'';
-		      console.log(msg);
-		      bot.sendMessage({
-		      	to: channelID,
-		      	message: ("LTC/USD : " + msg)
+		coinTicker('bittrex', 'LTC_BTC')
+   		.then((tick) => {
+      			console.log(tick)
+ 			console.log("LTC/BTC: " + tick.last);
+			bot.sendMessage({
+		      	  to: channelID,
+		      	  message: ("LTC/BTC: " + tick.last)
 		      });
-		  }
-		});
+   		})
 	}
-	
+
 	if (message==='.gno') {
-		var url = 'https://api.kraken.com/0/public/Ticker?pair=gnousd';
-		var msg='EMPTY';
-		request.get({
-			url: url,
-			json: true,
-			headers: {'User-Agent': 'request'}
-		}, (err, res, data) => {
-			if (err) {
-				console.log('Error:', err);
-				msg='Error '+ err;
-			} else if (res.statusCode !== 200) {
-				msg = 'Error Status code : '+res.statusCode;
-				console.log('Status:', res.statusCode);
-			} else {
-		      // data is already parsed as JSON:
-		      console.log(data.result.GNOUSD.a[0]);
-		      msg = data.result.GNOUSD.a[0]+'';
-		      console.log(msg);
-		      bot.sendMessage({
-		      	to: channelID,
-		      	message: ("GNO/USD : " + msg)
+		coinTicker('bittrex', 'GNO_BTC')
+   		.then((tick) => {
+      			console.log(tick)
+ 			console.log("GNO/BTC: " + tick.last);
+			bot.sendMessage({
+		      	  to: channelID,
+		      	  message: ("GNO/BTC: " + tick.last)
 		      });
-		  }
-		});
+   		})
 	}
-	
+
 	if (message==='.rep') {
-		var url = 'https://api.kraken.com/0/public/Ticker?pair=repusd';
-		var msg='EMPTY';
-		request.get({
-			url: url,
-			json: true,
-			headers: {'User-Agent': 'request'}
-		}, (err, res, data) => {
-			if (err) {
-				console.log('Error:', err);
-				msg='Error '+ err;
-			} else if (res.statusCode !== 200) {
-				msg = 'Error Status code : '+res.statusCode;
-				console.log('Status:', res.statusCode);
-			} else {
-		      // data is already parsed as JSON:
-		      console.log(data.result.XREPZUSD.a[0]);
-		      msg = data.result.XREPZUSD.a[0]+'';
-		      console.log(msg);
-		      bot.sendMessage({
-		      	to: channelID,
-		      	message: ("REP/USD : " + msg)
+		coinTicker('bittrex', 'REP_BTC')
+   		.then((tick) => {
+      			console.log(tick)
+ 			console.log("REP/BTC: " + tick.last);
+			bot.sendMessage({
+		      	  to: channelID,
+		      	  message: ("REP/BTC: " + tick.last)
 		      });
-		  }
-		});
+   		})
 	}
-	
+
 	if (message==='.xrp') {
-		var url = 'https://api.kraken.com/0/public/Ticker?pair=xrpusd';
-		var msg='EMPTY';
-		request.get({
-			url: url,
-			json: true,
-			headers: {'User-Agent': 'request'}
-		}, (err, res, data) => {
-			if (err) {
-				console.log('Error:', err);
-				msg='Error '+ err;
-			} else if (res.statusCode !== 200) {
-				msg = 'Error Status code : '+res.statusCode;
-				console.log('Status:', res.statusCode);
-			} else {
-		      // data is already parsed as JSON:
-		      console.log(data.result.XXRPZUSD.a[0]);
-		      msg = data.result.XXRPZUSD.a[0]+'';
-		      console.log(msg);
-		      bot.sendMessage({
-		      	to: channelID,
-		      	message: ("XRP/USD : " + msg)
+		coinTicker('bittrex', 'XRP_BTC')
+   		.then((tick) => {
+      			console.log(tick)
+ 			console.log("XRP/BTC: " + tick.last);
+			bot.sendMessage({
+		      	  to: channelID,
+		      	  message: ("XRP/BTC: " + tick.last)
 		      });
-		  }
-		});
+   		})
 	}
-	
+
+	if (message==='.gnt') {
+		coinTicker('bittrex', 'GNT_BTC')
+   		.then((tick) => {
+      			console.log(tick)
+ 			console.log("GNT/BTC: " + tick.last);
+			bot.sendMessage({
+		      	  to: channelID,
+		      	  message: ("GNT/BTC: " + tick.last)
+		      });
+   		})
+	}
+
+	if (message==='.strat') {
+		coinTicker('bittrex', 'STRAT_BTC')
+   		.then((tick) => {
+      			console.log(tick)
+ 			console.log("STRAT/BTC: " + tick.last);
+			bot.sendMessage({
+		      	  to: channelID,
+		      	  message: ("STRAT/BTC: " + tick.last)
+		      });
+   		})
+	}
+
+	if (message==='.sc') {
+		coinTicker('bittrex', 'SC_BTC')
+   		.then((tick) => {
+      			console.log(tick)
+ 			console.log("SC/BTC: " + tick.last);
+			bot.sendMessage({
+		      	  to: channelID,
+		      	  message: ("SC/BTC: " + tick.last)
+		      });
+   		})
+	}
+
+	if (message==='.doge') {
+		coinTicker('bittrex', 'DOGE_BTC')
+   		.then((tick) => {
+      			console.log(tick)
+ 			console.log("DOGE/BTC: " + tick.last);
+			bot.sendMessage({
+		      	  to: channelID,
+		      	  message: ("DOGE/BTC: " + tick.last)
+		      });
+   		})
+	}
+
 });
