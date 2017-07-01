2c4deeeca969e146620fdb0db2d73861c906863d
switchlove <switchbladelove@gmail.com>
2017-07-01 17:53:34 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 var Discord = require('discord.io');
+var coinTicker = require('coin-ticker');
 var request = require('request');
 var data = require("./config.json");
 
@@ -17,8 +18,8 @@ bot.on('message', function(user, userID, channelID, message, event) {
 	let prefix = ".";
 
 	if (message.startsWith(prefix + "ping")) {
-		bot.sendMessage({
-			to: channelID,
+    bot.sendMessage({
+      to: channelID,
 			message: "pong"
 		});
 	}
@@ -35,16 +36,35 @@ bot.on('message', function(user, userID, channelID, message, event) {
 		let coin = args[0].toUpperCase();
 		var url = 'https://min-api.cryptocompare.com/data/price?fsym=' + coin + '&tsyms=BTC,USD';
 		request({
-    			url: url,
-   			 json: true
+      url: url,
+      json: true
 		}, function (error, response, body) {
 			if (!error && response.statusCode === 200) {
-				bot.sendMessage({
-          to: channelID,
-          message: (coin + " ⇒ USD/BTC: $" + body.USD + " ฿" + body.BTC)
-			  });
-			}
+        var str2 = "_BTC";
+        var res = coin.concat(str2);
+        if(res == "BTC_BTC"){
+          var res = "BTC_USD"; 
+        } else
+          var priceb; var pricep; var pricediff;
+          coinTicker('bittrex', res)
+            .then((tick) => {
+            console.log("Bittrex: ฿" + tick.last)
+            priceb = tick.last
+          })
+          coinTicker('poloniex', res)
+            .then((tick2) => {
+            console.log("Poloniex: ฿" + tick2.last)
+            pricep = tick2.last
+          })
+          function doMessage() {
+            bot.sendMessage({
+              to: channelID,
+              message: ("Current value of " + coin + " ⇒ USD/BTC: $" + body.USD + " ฿" + body.BTC + "\n\n### Major Exchange Rates ###\nBittrex: ฿" + priceb + "\nPoloniex: ฿" + pricep)
+            });
+          }
+        }
+        setTimeout(doMessage, 600);
 		})
 	}
-	
+  
 });
