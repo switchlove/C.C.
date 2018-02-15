097c7ad6c5515e66f10cd0f86b3f74829eda6e97
Tim <switchbladelove@gmail.com>
2018-02-15 15:20:11 -0600

    Rewrite 002
    Updated to use discord.js.

--- index.js
+++ index.js
@@ -1,70 +1,67 @@
-var Discord = require('discord.io');
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
 var coinTicker = require('coin-ticker');
 var request = require('request');
-var data = require("./config.json");
+var logger = require('winston');
 
-botToken = data.botToken;
+const data = require("./config.json");
 
-var bot = new Discord.Client({
-  token: botToken,
-  autorun: true
+const token = data.botToken;
+
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}, with ${client.users.size} users, in ${client.channels.size} channels of ${client.guilds.size} guilds!`);
+  client.user.setPresence({ game: { name: 'crypto || !help', type: 0 } });
 });
 
-bot.on('ready', function() {
-	console.log('Logged in as %s - %s\n', bot.username, bot.id);
-});
-
-bot.on('message', function(user, userID, channelID, message, event) {
-	let prefix = ".";
-
-	if (message.startsWith(prefix + "ping")) {
-    bot.sendMessage({
-      to: channelID,
-			message: "pong"
-		});
-	}
-	
-	if (message.startsWith(prefix + "help")) {
-		bot.sendMessage({
-			to: channelID,
-			message: "Converts most cryptocurrencies to USD & BTC:\n .value COINHERE"
-		});	
-	}
-	
-	if (message.startsWith(prefix + "value")) {
-		let args = message.split(" ").slice(1);
-		let coin = args[0].toUpperCase();
-		var url = 'https://min-api.cryptocompare.com/data/price?fsym=' + coin + '&tsyms=BTC,USD';
-		request({
-      url: url,
-      json: true
-		}, function (error, response, body) {
-			if (!error && response.statusCode === 200) {
-        var str2 = "_BTC";
-        var res = coin.concat(str2);
-        if(res == "BTC_BTC"){
-          var res = "BTC_USD"; 
-        } else
-          var priceb; var pricep; var pricediff;
-          coinTicker('bittrex', res)
-            .then((tick) => {
-            console.log("Bittrex: ฿" + tick.last)
-            priceb = tick.last
-          })
-          coinTicker('poloniex', res)
-            .then((tick2) => {
-            console.log("Poloniex: ฿" + tick2.last)
-            pricep = tick2.last
-          })
-          function doMessage() {
-            bot.sendMessage({
-              to: channelID,
-              message: ("Current value of " + coin + " ⇒ USD/BTC: $" + body.USD + " ฿" + body.BTC + "\n\n### Major Exchange Rates ###\nBittrex: ฿" + priceb + "\nPoloniex: ฿" + pricep)
-            });
-          }
-        }
-        setTimeout(doMessage, 600);
-		})
-	}
+client.on("message", async message => {
+  if(message.author.bot) return;
+  if(message.content.indexOf(data.prefix) !== 0) return;
+  const args = message.content.slice(data.prefix.length).trim().split(/ +/g);
+  const command = args.shift().toLowerCase();
   
+  if(command === "ping") {
+    const m = await message.channel.send("Ping?");
+    m.edit(`Pong! Latency is ${m.createdTimestamp - message.createdTimestamp}ms. API Latency is ${Math.round(client.ping)}ms`);
+  }  
+  
+  if(command === "help") {
+    message.channel.send("Converts most cryptocurrencies to USD & BTC:\n !value ETH, this would look up Ethereum.")
+    .then(message => {message.delete(5000)}).catch(console.error);
+  }
+
+  if(command === "value") {
+	var coin = args[0].toUpperCase();
+	var url = 'https://min-api.cryptocompare.com/data/price?fsym=' + coin + '&tsyms=BTC,USD';
+	request({
+		url: url,
+		json: true
+	}, function (error, response, body) {
+		if (!error && response.statusCode === 200) {
+			var str2 = "_BTC";
+			var res = coin.concat(str2);
+			if(res == "BTC_BTC"){
+			  var res = "BTC_USD";
+			} else
+				var priceb; var pricep; var pricediff;
+				coinTicker('bittrex', res)
+					.then((tick) => {
+					console.log("Bittrex: ฿" + tick.last)
+					priceb = tick.last
+				})
+				coinTicker('poloniex', res)
+					.then((tick2) => {
+					console.log("Poloniex: ฿" + tick2.last)
+					pricep = tick2.last
+				})
+				function doMessage() {
+					message.channel.send("Current value of " + coin + " ⇒ USD/BTC: $" + body.USD + " ฿" + body.BTC + "\n\n### Major Exchange Rates ###\nBittrex: ฿" + priceb + "\nPoloniex: ฿" + pricep);
+				}
+			}
+		setTimeout(doMessage, 1500);
+	})
+  }
+
 });
+
+client.login(token);
